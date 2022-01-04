32c9c935fbe0afc9ca43b60b1cb1bd85587cbf03
Kate <fev3@mail.ru>
2022-01-04 09:44:35 +0100

    firebase deploy

--- src/sagas/editColumnSaga.js
+++ src/sagas/editColumnSaga.js
@@ -1,22 +1,24 @@
-import { takeEvery, put, select } from "redux-saga/effects";
+import { takeEvery, put, select, call } from "redux-saga/effects";
 
 import { EDIT_COLUMN, setLoading, setColumns } from "../actions";
 import { columnSelector, columnsSelector } from "../selectors";
 import * as Api from "./../api";
 
 function* editColumn(payload) {
+  const columns = yield select(columnsSelector);
   try {
     yield put(setLoading(true));
     const { columnId, columnTitle } = payload.column;
-    const columns = yield select(columnsSelector);
+
     const column = yield select(columnSelector(columnId));
     const newColumn = { ...column, title: columnTitle };
     //edit in state
     yield put(setColumns({ ...columns, [columnId]: { ...newColumn } }));
     // edit in database
-    yield Api.editColumn(columnId, columnTitle);
+    yield call(Api.editColumn, columnId, columnTitle);
   } catch (error) {
     console.log("error in patch: ", error);
+    yield put(setColumns({ ...columns }));
   }
   yield put(setLoading(false));
 }
