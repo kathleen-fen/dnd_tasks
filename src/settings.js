4a8bb807030df28d797ed08e372845e7814b7d7f
Kate <fev3@mail.ru>
2022-01-05 16:39:02 +0100

    env file added

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,5 @@
-export const Api_url = "https://tasks-2df6f-default-rtdb.firebaseio.com";
+export const Api_url =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL_DEV
+    : process.env.REACT_APP_API_URL_PROD;
 export const InitialColumnId = "-MrJWseBq4owGg_s87oW";
