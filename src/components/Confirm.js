e4faaedcd1c89d7f1a1b96ffcba32e1e9d701dfd
Kate <fev3@mail.ru>
2022-01-15 10:46:47 +0100

    confirm with mui

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Modal from "react-modal";
 import { useSelector, useDispatch } from "react-redux";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 import { confirmSelector, confirmStatusSelector } from "./../selectors";
 import { setConfirmStatus } from "./../actions";
@@ -12,28 +17,34 @@ export const Confirm = (props) => {
   const confirm = useSelector(confirmSelector);
   const confirmStatus = useSelector(confirmStatusSelector);
 
+  const handleCancel = () => {
+    dispatch(setConfirmStatus(false));
+  };
+
+  const handleConfirm = () => {
+    dispatch(confirm.act);
+    dispatch(setConfirmStatus(false));
+  };
+
   return (
-    <Modal isOpen={confirmStatus}>
-      <div>{confirm.message}</div>
-      <Button
-        variant="contained"
-        sx={{ m: 2 }}
-        onClick={() => {
-          dispatch(confirm.act);
-          dispatch(setConfirmStatus(false));
-        }}
+    <div>
+      <Dialog
+        open={confirmStatus}
+        onClose={handleCancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        Confirm
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ m: 2 }}
-        onClick={() => {
-          dispatch(setConfirmStatus(false));
-        }}
-      >
-        Cancel
-      </Button>
-    </Modal>
+        <DialogTitle id="alert-dialog-title">{"Delete item"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {confirm.message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleConfirm}>Confirm</Button>
+          <Button onClick={handleCancel}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
